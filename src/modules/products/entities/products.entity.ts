8725f8e56c5c95c1a6630464f34a3c1b6122f662
jacfpentester <jacfpentester@gmail.com>
2022-12-12 22:41:08 +0100

    entidaades

--- src/modules/products/entities/products.entity.ts
+++ src/modules/products/entities/products.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class Products {
     @PrimaryGeneratedColumn('uuid')
-    id:string;
+    id?:string;
 
     @Column('text', {unique:true})
     nombre:string;
